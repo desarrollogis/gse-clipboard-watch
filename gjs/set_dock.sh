b3ff4debe1c027b8483e6bc7d42d560491748e3c
Vicente Obregon <desarrollogis@gmail.com>
2023-02-28 07:58:33 -0700

    update

--- gjs/set_dock.sh
+++ gjs/set_dock.sh
@@ -7,8 +7,7 @@ X=$(echo "$INFO" | grep 'Absolute upper-left X:' | awk '{print $4}')
 Y=$(echo "$INFO" | grep 'Absolute upper-left Y:' | awk '{print $4}')
 WIDTH=$(echo "$INFO" | grep 'Width:' | awk '{print $2}')
 HEIGHT=$(echo "$INFO" | grep 'Height:' | awk '{print $2}')
-WIDTH=$((WIDTH+X-1))
-HEIGHT=$((HEIGHT+Y-1))
+WIDTH=$((X + WIDTH - 1))
+HEIGHT=$((Y + HEIGHT - 1))
 xprop -id "$XID" -format _NET_WM_STRUT_PARTIAL 32c -set _NET_WM_STRUT_PARTIAL "$X,$Y,$HEIGHT,0,0,0,0,0,0,$WIDTH,0,0"
-echo xprop -id "$XID" -format _NET_WM_STRUT_PARTIAL 32c -set _NET_WM_STRUT_PARTIAL "$X,$Y,$HEIGHT,0,0,0,0,0,0,$WIDTH,0,0"
 exit 0
